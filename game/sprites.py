fe885d3f44215b84bc089fc4d853028419c6e88f
Callum Donnelly <callum.donnelly74@gmail.com>
2025-03-06 16:25:01 +0000

    tiled graphics

--- game/sprites.py
+++ game/sprites.py
@@ -8,7 +8,7 @@ import pytmx
 #tmxdata = pytmx.TiledMap("game\city1.tmx")
 
 from pytmx.util_pygame import load_pygame
-tmxdata = pytmx.TiledMap("game\city1.tmx")
+tmxdata = pytmx.TiledMap('game/city1.tmx')
 
 
 image = tmx_data.get_tile_image(0, 0, layer)
