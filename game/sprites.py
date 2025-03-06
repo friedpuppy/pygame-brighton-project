c308027563283a3724914d6e65911c8f1d050218
Callum Donnelly <callum.donnelly74@gmail.com>
2025-03-06 16:40:27 +0000

    .

--- game/sprites.py
+++ game/sprites.py
@@ -8,10 +8,10 @@ import pytmx
 #tmxdata = pytmx.TiledMap("game\city1.tmx")
 
 from pytmx.util_pygame import load_pygame
-tmx_data = pytmx.TiledMap('game/city1.tmx')
+tmx_data = pytmx.TiledMap('game/tiledmaps/city1.tmx')
 
 
-image = tmx_data.get_tile_image(0, 0, 'Ground')
+image = tmx_data.get_tile_image(0, 0, layer)
 screen.blit(image, position)
 
 class Spritesheet:
