545e77b2a2f9429feea616dc776d08d577be4650
Callum Donnelly <callum.donnelly74@gmail.com>
2025-03-07 12:23:50 +0000

    map graphics

---
+++ game/map/Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].tsx
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Slates v.2 [32x32px orthogonal tileset by Ivan Voirol]" tilewidth="32" tileheight="32" tilecount="1288" columns="56">
+ <image source="D:/Downloads/Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].png" width="1792" height="736"/>
+ <tile id="108">
+  <animation>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="109" duration="100"/>
+   <frame tileid="110" duration="100"/>
+   <frame tileid="111" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="664">
+  <animation>
+   <frame tileid="664" duration="100"/>
+   <frame tileid="832" duration="100"/>
+   <frame tileid="1000" duration="100"/>
+   <frame tileid="1168" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="665">
+  <animation>
+   <frame tileid="665" duration="100"/>
+   <frame tileid="833" duration="100"/>
+   <frame tileid="1001" duration="100"/>
+   <frame tileid="1169" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="666">
+  <animation>
+   <frame tileid="666" duration="100"/>
+   <frame tileid="778" duration="100"/>
+   <frame tileid="890" duration="100"/>
+   <frame tileid="1002" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="667">
+  <animation>
+   <frame tileid="667" duration="100"/>
+   <frame tileid="779" duration="100"/>
+   <frame tileid="891" duration="100"/>
+   <frame tileid="1003" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="720">
+  <animation>
+   <frame tileid="720" duration="100"/>
+   <frame tileid="888" duration="100"/>
+   <frame tileid="1056" duration="100"/>
+   <frame tileid="1224" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="721">
+  <animation>
+   <frame tileid="721" duration="100"/>
+   <frame tileid="889" duration="100"/>
+   <frame tileid="1057" duration="100"/>
+   <frame tileid="1225" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="722">
+  <animation>
+   <frame tileid="722" duration="100"/>
+   <frame tileid="834" duration="100"/>
+   <frame tileid="946" duration="100"/>
+   <frame tileid="1058" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="723">
+  <animation>
+   <frame tileid="723" duration="100"/>
+   <frame tileid="835" duration="100"/>
+   <frame tileid="947" duration="100"/>
+   <frame tileid="1059" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="744">
+  <animation>
+   <frame tileid="744" duration="100"/>
+   <frame tileid="745" duration="100"/>
+   <frame tileid="746" duration="100"/>
+   <frame tileid="747" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="776">
+  <animation>
+   <frame tileid="776" duration="100"/>
+   <frame tileid="944" duration="100"/>
+   <frame tileid="1112" duration="100"/>
+   <frame tileid="1280" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="777">
+  <animation>
+   <frame tileid="777" duration="100"/>
+   <frame tileid="945" duration="100"/>
+   <frame tileid="1113" duration="100"/>
+   <frame tileid="1281" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1212">
+  <animation>
+   <frame tileid="1212" duration="100"/>
+   <frame tileid="1213" duration="100"/>
+   <frame tileid="1214" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
