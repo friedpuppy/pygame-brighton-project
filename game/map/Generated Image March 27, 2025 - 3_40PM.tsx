b051663e37e6dc403d7700d28abf7f99c0111e27
Callum Donnelly <callum.donnelly74@gmail.com>
2025-03-28 19:50:09 +0000

    upload graphics

---
+++ game/map/Generated Image March 27, 2025 - 3_40PM.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Generated Image March 27, 2025 - 3_40PM" tilewidth="62" tileheight="102" tilecount="1" columns="1">
+ <image source="D:/Documents/python/brighton/test-python/game/img/piermaster1.png" trans="ff00ff" width="62" height="102"/>
+</tileset>
